d5d89646098debff45304041a9cc4aa13a778bd2
mcfrojd <mcfrojd@gmail.com>
2020-10-19 17:24:53 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 echo ""
-IP="hostname -I | cut -d' ' -f2"
-echo $IP
-echo ""
-echo "Access Portainer on xxx.xxx.xxx.xxx:9000"
+IP=`hostname -I | cut -d' ' -f2`
+echo "Access Portainer on " $IP":9000"
 echo ""
