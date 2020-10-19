11a9d068348dc9837a3b72e45a97cd0bbb9fe7c4
mcfrojd <mcfrojd@gmail.com>
2020-10-19 16:41:52 +0200

    more updates

--- testscript.sh
+++ testscript.sh
@@ -7,6 +7,7 @@ echo "Give Root login access"
 echo ""
 sleep 2
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+echo ""
 echo "Give Root login access - successful"
 sleep 5
 
@@ -17,6 +18,7 @@ echo "Run apt update"
 echo ""
 sleep 2
 sudo apt update -y
+echo ""
 echo "Run apt update - successful"
 sleep 5
 
@@ -26,6 +28,7 @@ echo "Run apt upgrade"
 echo ""
 sleep 2
 sudo apt upgrade -y
+echo ""
 echo "Run apt upgrade - successful"
 sleep 5
 
@@ -36,6 +39,7 @@ echo "Install curl"
 echo ""
 sleep 2
 sudo apt install curl -y
+echo ""
 echo "Install curl - successful"
 sleep 5
 
@@ -45,6 +49,7 @@ echo "Get docker script"
 echo ""
 sleep 2
 curl -fsSL https://get.docker.com -o get-docker.sh
+echo ""
 echo "Get docker script - successful"
 sleep 5
 
@@ -54,6 +59,7 @@ echo "Run docker script"
 echo ""
 sleep 2
 sudo sh get-docker.sh
+echo ""
 echo "Run docker script - successful"
 sleep 5
 
@@ -61,16 +67,53 @@ sleep 5
 # Make some update to get the latest version
 clear
 echo ""
-echo "Get docker-compose"
+echo "Get docker-compose version 1.24.7"
 echo ""
 sleep 2
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo ""
 echo "Get docker-compose - successful"
 sleep 5
-#sudo chmod +x /usr/local/bin/docker-compose
-#clear
-#docker-compose --version
+
+clear
+echo ""
+echo "Give docker-compose +x rights"
+echo ""
+sleep 2
+sudo chmod +x /usr/local/bin/docker-compose
+echo ""
+echo "Give docker-compose +x rights - successful"
+sleep 5
+
+clear
+echo ""
+echo "Verify docker-compose version"
+echo ""
+sleep 2
+docker-compose --version
+echo ""
+echo "Verify docker-compose version - successful"
+sleep 5
 
 # Install portainer
-#docker volume create portainer_data
-#docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+clear
+echo ""
+echo "Create Portainer Volume"
+echo ""
+sleep 2
+docker volume create portainer_data
+echo ""
+echo "Create Portainer Volume - successful"
+sleep 5
+
+clear
+echo ""
+echo "Install Portainer"
+echo ""
+sleep 2
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+echo ""
+echo "Install Portainer - successful"
+sleep 5
+echo ""
+echo "Script successfully executed"
